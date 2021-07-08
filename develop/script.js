f2a6164085daf2452b2b25a7c704a379c50318dd
Ray a <rayeed.ashir@gmail.com>
2021-07-08 11:08:26 -0700

    added loop to insert all rows and show the time

--- develop/script.js
+++ develop/script.js
@@ -12,4 +12,47 @@ const currentDayEl = document.getElementById("currentDay");
 
 currentDayEl.textContent = moment().format("dddd, MMM Do");
 
-console.log(moment().format("dddd"));
+function addRow(time) {
+    //TODO: create the elements using jQuery
+    //create a row element
+    let currentHour = moment().format("h");
+    let presentState = "";
+    if(time < currentHour) {
+        presentState = "past";
+    } else if (time == currentHour) {
+        presentState = "present";
+    } else {
+        presentState = "future";
+    }
+
+    let rowEl = document.createElement("div");
+    rowEl.classList.add("time-block", presentState, "row");
+
+    let hourEl = document.createElement("div");
+    hourEl.classList.add("hour","col-xl-1");
+    hourEl.textContent = time < 12 ? time + ":00 AM" : time +":00 PM";
+
+    let textAreaEl = document.createElement("textarea");
+    textAreaEl.classList.add("col-xl-10");
+
+    let buttonEl = document.createElement("button");
+    buttonEl.classList.add("saveBtn", "col-xl-1", "fas", "fa-save");
+
+    rowEl.appendChild(hourEl);
+    rowEl.appendChild(textAreaEl);
+    rowEl.appendChild(buttonEl);
+
+    $(".container").append(rowEl);
+
+    //timeSlotContainerEl.appendChild(rowEl);
+    
+    //add time to row
+    //add text area to row
+    //add save button to row
+    
+}
+
+for (let i = 8; i < 19; i++) {
+    addRow(i)
+    // console.log(i > 12 ? i-12 : i);
+}
