8c7278288889fcff357d3c3b65a6261b5564bbff
Ray a <rayeed.ashir@gmail.com>
2021-07-08 08:51:00 -0700

    added date to calendar

---
+++ develop/script.js
@@ -0,0 +1,13 @@
+/* 
+1. display the day at the top of the calendar -Done
+2. display time blocks of 1hr
+handle blcok clicks
+display save button 
+handle save button 
+handle refresh
+*/
+const currentDayEl = document.getElementById("currentDay");
+
+currentDayEl.textContent = moment().format("dddd, MMM Do");
+
+console.log(moment().format("dddd"));
